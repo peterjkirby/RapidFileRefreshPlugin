7d31cb9f4296520dc9be05120dc5bb05f54344e7
Peter Kirby <peterjkirby@live.com>
2016-04-11 10:40:39 -0700

    initial

--- src/rfr/core/ProjectMonitorJob.java
+++ src/rfr/core/ProjectMonitorJob.java
@@ -14,15 +14,16 @@ import org.eclipse.core.runtime.jobs.Job;
 public class ProjectMonitorJob extends Job {
 
 	private static final String JOB_NAME = "ProjectMonitorJob";
-	
-	// add folders here to monitor them (folder that exist within the projects you are monitoring)
+
+	// add folders here to monitor them (folder that exist within the projects
+	// you are monitoring)
 	private static final String[] FOLDERS = {
 			"src/main/webapp/resources"
 	};
 	// fires about 3 times per second.
 	private static final int DELAY = 350;
 	private boolean RUNNING = true;
-	
+
 	private IResource resource;
 	private IRefreshResult result;
 
@@ -43,23 +44,24 @@ public class ProjectMonitorJob extends Job {
 			for (String folderPath : FOLDERS) {
 				monitor(project, folderPath);
 			}
-			
+
 			// schedule the next run
 			schedule(DELAY);
 		}
 
 		return Status.OK_STATUS;
 	}
-	
+
 	private void monitor(IProject project, String folderPath) {
 		IFolder folder = project.getFolder(new Path(folderPath));
-		
+
 		if (folder.exists()) {
 			ResourceTraverser traverser = new ResourceTraverser(new ResourceChangeEvaluator());
 
 			try {
 				traverser.traverse(folder, result);
-			} catch (CoreException e) {}
+			} catch (CoreException e) {
+			}
 		}
 	}
 
--- src/rfr/core/ResourceTraverser.java
+++ src/rfr/core/ResourceTraverser.java
@@ -9,42 +9,50 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
 public class ResourceTraverser {
-	
+
 	private ResourceChangeEvaluator resourceChangeEvaluator;
-	
+
 	public ResourceTraverser(ResourceChangeEvaluator resourceChangeEvaluator) {
 		this.resourceChangeEvaluator = resourceChangeEvaluator;
 	}
-	
+
 	public void traverse(IFolder folder, IRefreshResult refreshResult) throws CoreException {
 
 		IResource[] contents = folder.members();
 		Activator activator = Activator.getInstance();
-		
+
 		if (resourceChangeEvaluator.changed(folder)) {
 			refreshResult.refresh(folder);
 		}
-		
-		for (IResource resource : contents) {									
+
+		for (IResource resource : contents) {
 			if (resource.getType() == IResource.FILE) {
-				IFile file = (IFile) resource;				
-									
-					if (resourceChangeEvaluator.changed(file)) {
-						refreshResult.refresh(file);
-						activator.getLog().log(new Status(IStatus.INFO, Activator.PLUGIN_ID, "File change detected. Refreshing " + file.getName()));
-					}
-				
-				
+				IFile file = (IFile) resource;
+
+				if (resourceChangeEvaluator.changed(file)) {
+					refreshResult.refresh(file);
+					activator.getLog().log(
+							new Status(IStatus.INFO,
+									Activator.PLUGIN_ID,
+									"File change detected. Refreshing " + file.getName()));
+				}
+
 			} else if (resource.getType() == IResource.FOLDER) {
 				IFolder subfolder = (IFolder) resource;
-				
-				// only continue traversing if a folder has not changed. If a folder has changed
-				// the refresh event at that folder should be enough to force synchronization 
-				// Plus, other changes at a depth > 1 will be detected on the next pass.
-				// This is more a performance optimization than anything else. 
+
+				// only continue traversing if a folder has not changed. If a
+				// folder has changed
+				// the refresh event at that folder should be enough to force
+				// synchronization
+				// Plus, other changes at a depth > 1 will be detected on the
+				// next pass.
+				// This is more a performance optimization than anything else.
 				if (resourceChangeEvaluator.changed(subfolder)) {
 					refreshResult.refresh(subfolder);
-					activator.getLog().log(new Status(IStatus.INFO, Activator.PLUGIN_ID, "Folder change detected. Refreshing " + subfolder.getName()));
+					activator.getLog().log(
+							new Status(IStatus.INFO,
+									Activator.PLUGIN_ID,
+									"Folder change detected. Refreshing " + subfolder.getName()));
 				} else {
 					traverse((IFolder) resource, refreshResult);
 				}
